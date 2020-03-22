d8e8d4d8c91f656127ecafafe62c3b7cdbe76af9
Radim Bureš <radim.bures@gmail.com>
2020-03-22 16:33:28 +0100

    update tym, ucastnici

--- lide.php
+++ lide.php
@@ -37,16 +37,36 @@ Do the following if you're using your customized build of modernizr (http://www.
   </div>
   <div class="body">
       <div id="menu" class="underbody">
-  <p><a href="index.html">Novinky</a>  ∞  <a href="kurz.html">Kurz</a>  ∞  <a href="termin.html">Termín & Podmínky</a>  ∞  <a href="katedry.html">Katedry</a>  ∞  <a href="lide.html">Účastníci</a>  ∞  <a href="tym.html">Tým</a></p>  </div>
+  <p><a href="index.html">Novinky</a>  ∞  <a href="kurz.html">Kurz</a>  ∞  <a href="terminy.html">Termíny</a>  ∞  <a href="katedry.html">Katedry</a>  ∞  <a href="podminky.html">Podmínky</a>  ∞  <a href="tym.html">Tým</a></p>  </div>
     
     
     
     <div class="underbody">
+	<?php
+require 'login.php';
+
+if (isset($_COOKIE['PrivatePageLogin'])) {
+   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
+?>
+	
+	
       <h2>Účastníci</h2>
 	<h3>Koho na Ventu potkáš?</h3>
     </div>
 	
+	<!--
+	<div class="underbody">
+	<p>
+	Ondra <b>Hoblík</b> Růžička</p>
+	<p style="text-align: right">
+		oddíl Krhúti<br></p>
+		<p style="text-align: justify">
+		<a href="img/2018/2018_Ondra.jpg"><img style="float: left; margin-right: 10px" src="img/2018/2018_Ondra.jpg" height="250" alt="ondra" ></a>Do junáka jsem vstoupil ve svých osmi letech, mým domovským oddílem je 76. oddíl Krhúti v  Praze 4. Poslední čtyři roky připravuji program pro vlčata. V oddíle mi říkají Hoblík, protože jsem profesí umělecký truhlář. V našem oddíle se mi libí pestrost programu, soužití s přírodou, turistika, vodáctví, cyklistika, zimní sporty.  
+	<br><br><br><br><br>
+	</p>
+	</div>
 
+-->
 
 	
 	
@@ -60,5 +80,36 @@ Do the following if you're using your customized build of modernizr (http://www.
     </div>
   </div>
 </div>
+<?php
+      exit;
+   } else {
+      echo "Bad Cookie.";
+      exit;
+   }
+}
+
+if (isset($_GET['p']) && $_GET['p'] == "login") {
+   if ($_POST['user'] != $username) {
+      echo "Špatně zadané jméno. Klikni pro <a href=lide.php>návrat</a>.";
+      exit;
+   } else if ($_POST['keypass'] != $password) {
+      echo "Špatně zadané heslo. Klikni pro <a href=lide.php>návrat</a>.";
+      exit;
+   } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
+      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
+      header("Location: $_SERVER[PHP_SELF]");
+   } else {
+      echo "Nelze se přihlásit.";
+   }
+}
+?>
+
+<h2>Přihlášení</h2>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
+<label><input type="text" name="user" id="user" /> Zadej jméno</label><br />
+<label><input type="password" name="keypass" id="keypass" /> Zadej heslo</label><br /><br />
+<input type="submit" id="submit" value="Přihlásit se" /><br /><br />
+</form>
+
 </body>
 </html>
